30c21a5cb1b1e384df871f8d297ac4997db3f094
Luyt Bui <luytbq43@gmail.com>
2021-12-22 10:34:08 +0700

    fix bug

--- Canvas.js
+++ Canvas.js
@@ -2,7 +2,7 @@ function randomColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-function clearCanvas(context) {
+function clearCanvas(canvas, context) {
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
